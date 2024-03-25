5bc5a4809f4d6840659d2c87da4bc37179a565ed
Justine Saint-Martin <justine.saint-martin@laplateforme.io>
2024-03-25 18:46:19 +0100

    create an array with a for loop

---
+++ jour01/job05/script.js
@@ -0,0 +1,12 @@
+function afficherJoursSemaine() {
+    // Créer un tableau 
+    var joursSemaine = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+
+    // Boucle 
+    for (var i = 0; i < joursSemaine.length; i++) {
+        console.log(joursSemaine[i]); 
+    }
+}
+
+
+afficherJoursSemaine();
