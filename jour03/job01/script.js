b00798acc4d80c4e68b2101f8dcaeb3e6e48e6b3
Justine Saint-Martin <justine.saint-martin@laplateforme.io>
2024-03-27 10:40:52 +0100

    create button version jquery

---
+++ jour03/job01/script.js
@@ -0,0 +1,20 @@
+function showhide() {
+    var $clic = $("#article");
+    var $texte
+  
+    if ($clic.length) {
+      
+      if ($clic.is(":visible")) {
+        $clic.hide();
+      } else {
+        $clic.show();
+      }
+    } else {
+    
+      $clic = $("<clic></article>")
+        .attr("id", "article")
+        .text("Les logiciels et les cathédrales, c'est un peu la même chose d'abord, on les construit, ensuite, on prie.")
+        .appendTo("body");
+    }
+  }
+  
