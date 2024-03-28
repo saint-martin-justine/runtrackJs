db69642f4aaa8dd6a023f1e28918e9d39b2006f5
Justine Saint-Martin <justine.saint-martin@laplateforme.io>
2024-03-28 13:17:30 +0100

    I created a json string format

--- jour04/job02/script.js
+++ jour04/job02/script.js
@@ -1,25 +1,16 @@
-function jsonValueKey(jsonString, key) {
-    try {
-      
-      const obj = JSON.parse(jsonString);
-  
-     
-      return obj[key];
-    } catch (e) {
-      
-      console.error("Erreur lors de l'analyse du JSON : ", e);
-    }
-  }
-  
+let para = document.getElementById("ville");
+let button = document.getElementById("button");
 
-  const jsonString = `{
-      "name": "La Plateforme",
-      "address": "8 rue d'hozier",
-      "city": "Marseille",
-      "nb_staff": "11",
-      "creation": "2019"
-  }`;
-  
-  const key = "city";
-  console.log(jsonValueKey(jsonString, key)); 
-  
+
+async function jsonValueKey() {
+   
+    button.addEventListener("click", () => {
+      const url = "plateforme.json"
+      fetch(url)
+      .then(response => response.json())
+      .then(data => para.innerHTML = data["city"])
+    })
+}
+
+
+jsonValueKey()
